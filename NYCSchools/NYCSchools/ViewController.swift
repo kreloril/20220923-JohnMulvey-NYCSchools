44b17b84c5458e91266aa51a3b594618f5d72fdb
John Mulvey <jcmulvey@gmail.com>
2022-09-23 15:51:58 -0700

    Initial Commit, of project

---
+++ NYCSchools/NYCSchools/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  NYCSchools
+//
+//  Created by John Mulvey on 9/23/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.navigationItem.title = "NYCSchools"
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
