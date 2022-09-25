bfb1ec288f11ebab371d03836f755b3add71dfdf
John Mulvey <jcmulvey@gmail.com>
2022-09-24 19:58:31 -0700

    NYC-7, NYC-8, NYC-9 | Implemented first with with all schools

--- Presenter/SchoolPresenter.swift
+++ Presenter/SchoolPresenter.swift
@@ -49,17 +49,65 @@ class SchoolPresenter: NSObject, UITableViewDelegate, UITableViewDataSource {
  */
 
 extension SchoolPresenter  {
+
+    func tableView(_ tableView: UITableView, heightForHeaderInSection:Int) -> Int {
+        return 1
+    }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        if let objects = self.viewModelSchools {
+            return 1
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        
+    }
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        if let objects = viewModelSchools {
             return objects.count
         }
         
-        return 0
+        return 0;
+    }
+    
+    func tableView(_ tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
+      
+        let view = UIView()
+        view.backgroundColor = UIColor.clear
+        return view
+    }
+    
+    func tableView(_ tableView: UITableView, heightForFooterInSection section: Int) -> CGFloat {
+        return 2.0
+    }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 125
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cellSchool", for: indexPath) as? NYCSchoolTableViewCell
+        
+        if let objects = self.viewModelSchools, let cell = cell {
+   
+            let school = objects[indexPath.section]
+            
+            cell.labelAddress.textColor = UIColor.black
+            cell.labelAddress.text = school.address
+            
+            cell.labelTitle.text = school.schoolName
+            cell.labelTitle.textColor = UIColor.orange
+            cell.viewContent.backgroundColor = UIColor.systemGroupedBackground
+            cell.viewContent.layer.cornerRadius = 6.0
+            cell.viewContent.layer.borderColor = UIColor.green.cgColor
+            cell.viewContent.layer.borderWidth = 2.0
+            return cell
+        }
+        
+        
         return UITableViewCell()
+        
     }
     
 }
--- ViewController/NYCSchoolViewController.swift
+++ ViewController/NYCSchoolViewController.swift
@@ -9,10 +9,18 @@ import UIKit
 
 class NYCSchoolViewController: UIViewController {
 
+    var presenter:SchoolPresenter?
+    
+    @IBOutlet weak var tableView: UITableView!
     override func viewDidLoad() {
         super.viewDidLoad()
         self.navigationItem.title = "NYCSchools"
         // Do any additional setup after loading the view.
+        self.presenter = SchoolPresenter(dataSourceSchools: "f9bf-2cp4-schools", dataSourceScores: "f9bf-2cp4-sat")
+        
+        self.tableView.dataSource = self.presenter
+        self.tableView.delegate = self.presenter
+        
     }
 
 
